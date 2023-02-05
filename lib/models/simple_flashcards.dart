2b67270f60264358b9870aa5191d12828c8ce0a6
Krille <christian-pauly@posteo.de>
2023-02-05 11:38:58 +0100

    chore: follow up hive fix

--- lib/models/simple_flashcards.dart
+++ lib/models/simple_flashcards.dart
@@ -20,7 +20,7 @@ class SimpleFlashcards {
   SimpleFlashcards(this.stacksBox, this.preferences);
 
   static Future<SimpleFlashcards> init() async {
-    await Hive.initFlutter('debug');
+    await Hive.initFlutter();
     final box = await Hive.openBox(AppConstants.dbName);
     final preferences = await SharedPreferences.getInstance();
     return SimpleFlashcards(box, preferences);
