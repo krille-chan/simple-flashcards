aaf1dcd2ad61e3153429dc78ff01b4fcf01b2fa8
Krille <christian-pauly@posteo.de>
2023-12-07 07:06:29 +0100

    build: Update dependencies

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/krille/Applications/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/krille/HADTF/Flutter/simple_flashcards"
+export "FLUTTER_ROOT=/Users/christianpauly/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/christianpauly/develop/simple-flashcards"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.1.0"
